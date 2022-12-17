b0144e472bd3501fd462fda8310fb1f59d363dc1
Manuel Rivera <menu.rivera@gmail.com>
2022-12-17 00:45:10 -0600

    [INIT]

---
+++ index.js
@@ -0,0 +1,16 @@
+const app = require('express')()
+
+app.post('/log', (req, res) => {
+    const { type, data, action } = req.body
+    console.log({
+        type,
+        data,
+        action
+    });
+
+    res.status(200)
+})
+
+app.listen(3000, () => {
+    console.log('Logger listening on port 3000');
+})
