19dfeee4f0995cf941608da41d6220d2957607af
Manuel Rivera <menu.rivera@gmail.com>
2023-11-22 17:19:21 -0600

    [MOD] -> CORS setup

--- api/log.ts
+++ api/log.ts
@@ -1,6 +1,16 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node"
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
+	// cors setup
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+	)
+	if (req.method === 'OPTIONS') {
+		return res.status(200).end()
+	}
 
 	console.log({
 		method: req.method,
